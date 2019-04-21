8fa8e91ba793a7c60f645fe6e03bf2c18782c0de
shouldnotappearcalm <1191465097@qq.com>
2019-04-22 00:42:53 +0800

    feat: swagger自动同步

---
+++ server.js
@@ -0,0 +1,33 @@
+const controller = require('./controller/syncController.js');
+const yapi =require('yapi.js');
+const interfaceSyncUtils = require('./interfaceSyncUtils.js');
+
+module.exports = function () {
+  yapi.getInst(interfaceSyncUtils);
+
+  this.bindHook('add_router', function (addRouter) {
+    addRouter({
+      controller: controller,
+      method: 'get',
+      path: 'autoSync/get',
+      action: 'getSync'
+    });
+    addRouter({
+      controller: controller,
+      method: 'post',
+      path: 'autoSync/save',
+      action: 'upSync'
+    });
+    addRouter({
+      /**
+       * 保存期望
+       */
+      controller: controller,
+      method: 'post',
+      path: 'autoSync/upd',
+      action: 'saveCase'
+    });
+
+  });
+
+};
